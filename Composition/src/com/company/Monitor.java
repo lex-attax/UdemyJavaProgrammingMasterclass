078d47d9d7a1ae1bd2fb7ae339531d7765218d75
lex-attax <lex.pavlovic@gmail.com>
2019-04-20 13:44:13 +0200

    composition exercise

---
+++ Composition/src/com/company/Monitor.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResosution;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResosution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResosution = nativeResosution;
+    }
+
+    public void drawPixelAt(int x, int y, String color) {
+        System.out.println("Drawing pixel at " +x + "," + y + " in colour " + color);
+    }
+}
